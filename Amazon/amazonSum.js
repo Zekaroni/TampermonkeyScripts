5af7cbdedf7c0cdaa2848ee828eb6ca377bd337c
Zekaroni <ezekiel.hall.04@gmail.com>
2024-01-02 14:43:32 -0500

    added looping function

--- Amazon/amazonSum.js
+++ Amazon/amazonSum.js
@@ -4,7 +4,7 @@
 // @version      v1.31
 // @description  Shows the sum of all items in an Amazon list.
 // @author       Zekaroni
-// @match        https://www.amazon.com/hz/wishlist/ls/*
+// @match        https://www.amazon.com/hz/wishlist/*
 // @icon         data:image/gif;base64,R0lGODlhEAAQAPQAAAAAAGRvkmVwk2p1lmt1lnB5mXF6mXZ/nHuEn3yEnnyEn4KJoYKJooiOpIiOpY2Spo6Tpo2SqI6SqI6TqJSXqZSYqpmdrJmdrZ+ir5+isKSnsqWms6WnswAAAAAAAAAAACH5BAEAAAAALAAAAAAQABAAAAVBICCOZGmeqKitWjpiWYa5omVbNEBVFYVKwKAQAiE5jsjkkcRoOp9NkmKqQEytVNRhe8gZvobcYDzIBQSCQG69DgEAOw==
 // @grant        none
 // @homepage     https://github.com/Zekaroni/TampermonkeyScripts
@@ -16,24 +16,30 @@
     'use strict';
     var listItems = document.querySelector("#g-items").children;
     var totalDisplay = document.querySelector("#profile-list-name");
-    var itemValue = 0;
+    var listName = totalDisplay.textContent
     var listSum = 0;
+    var itemValue = 0;
     var itemPriorityBox;
-    for (let i = 0; i < listItems.length; i++)
+    function updateTotalDisplay()
     {
-        itemValue = listItems[i].getAttribute("data-price");
-        if (itemValue !== null)
+        for (let i = 0; i < listItems.length; i++)
         {
-            if (itemValue != "-Infinity")
+            itemValue = listItems[i].getAttribute("data-price");
+            if (itemValue !== null)
             {
-                itemPriorityBox = listItems[i].querySelector(".a-fixed-right-grid-col").querySelectorAll(".a-column")[1]
-                if (itemPriorityBox.classList.contains("a-hidden") || !(!itemPriorityBox.classList.contains("a-hidden") && itemPriorityBox.querySelector(".dropdown-priority").classList.contains("item-priority-lowest")))
+                if (itemValue != "-Infinity")
                 {
-                    itemValue = Number(itemValue);
-                    listSum+=itemValue;
+                    itemPriorityBox = listItems[i].querySelector(".a-fixed-right-grid-col").querySelectorAll(".a-column")[1]
+                    if (itemPriorityBox.classList.contains("a-hidden") || !(!itemPriorityBox.classList.contains("a-hidden") && itemPriorityBox.querySelector(".dropdown-priority").classList.contains("item-priority-lowest")))
+                    {
+                        itemValue = Number(itemValue);
+                        listSum+=itemValue;
+                    };
                 };
             };
         };
+        totalDisplay.textContent = listName + " | Total : $" + listSum.toFixed(2);;
+        setTimeout(updateTotalDisplay, 1000);
     };
-    totalDisplay.textContent += " | Total : $" + listSum.toFixed(2);;
+    updateTotalDisplay();
 })();
